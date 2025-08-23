df2fe35f50fd9df81d445da57bd4409570849dec
Rohith.M <rohithmerugu2423@gmail.com>
2025-08-23 09:32:03 +0530

    Add files via upload

---
+++ Lab_5/src/Codes/Transportation.java
@@ -0,0 +1,36 @@
+package Codes;
+import java.util.*;
+public class Transportation 
+{
+	public static void main(String[] args)
+	{
+		        Transport t1 = new Bus();
+		        Transport t2 = new Train();
+		        t1.ticketPrice();
+		        t2.ticketPrice();
+		    }
+		}
+
+		class Transport 
+		{
+		    void ticketPrice()
+		    {
+		        System.out.println("Ticket price for transport");
+		    }
+		}
+
+		class Bus extends Transport 
+		{
+		    void ticketPrice() 
+		    {
+		        System.out.println("Bus ticket price is $15");
+		    }
+		}
+
+		class Train extends Transport
+		{
+		    void ticketPrice() 
+		    {
+		        System.out.println("Train ticket price is $35");
+		    }
+		}
